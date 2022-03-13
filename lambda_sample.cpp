c2aed12d5c32bbf12dcc20a1375e3ae014545c34
YongHo Kang <ca3yh@hotmail.com>
2022-03-13 16:48:33 +0900

    the first function "TestFunc"

--- lambda_sample.cpp
+++ lambda_sample.cpp
@@ -2,10 +2,24 @@
 
 int TestFunc(int param)
 {
-    std::cout << "Function Pointer"
+    std::cout << "Function Pointer" << param << std::endl;
+    return param;
 }
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    auto func = [](int param) -> int
+    {
+        std::cout << "Lambda: " << param << std::endl;
+        return param;
+    };
+
+    //call lambda
+    func(5);
+
+    //call by function pointer
+    auto functionPointer = TestFunc;
+    functionPointer(10);
+
+    return 0;
 }
